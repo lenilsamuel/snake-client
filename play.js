58dc8806c26690bc99fbfc34e645c04f9ee68a35
lenilsamuel <lenilvarghese@gmail.com>
2021-11-07 15:50:13 -0700

    mapped ctr + c to terminate the program

--- play.js
+++ play.js
@@ -1,19 +1,21 @@
-const net = require("net");
+// const net = require("net");
+const {connect} = require('./client');
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on("data", (data) => {
-    console.log(`Message from server - ${data}`);
-  });
-  return conn;
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  const handleUserInput = function (key) {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  };
+  
+  stdin.on("data", handleUserInput);
+  return stdin;
 };
 
 console.log("Connecting ...");
 connect();
+setupInput();
