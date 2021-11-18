c10a1d19954234db22c88a76869d38edd572fe8b
lenilsamuel <lenilvarghese@gmail.com>
2021-11-17 20:09:14 -0700

    added code to send name to server

--- client.js
+++ client.js
@@ -9,19 +9,15 @@ const connect = function () {
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write('Name: LS');
+  });
+  
   conn.on("data", (data) => {
     console.log(`Message from server - ${data}`);
   });
 
-  conn.on("connect", () => {
-    console.log("Successfully connected to game server");
-    conn.write('Name: LS');
-    // conn.write("Say: Mama Mia!");
-    // conn.write('Move: up');
-    // setInterval(() => {
-    //   conn.write('Move: right');
-    // }, 100);
-  });
   return conn;
 };
 
