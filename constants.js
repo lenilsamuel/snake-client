b819c4de1cd97191e0e715fc4f96a4403f3d6e53
lenilsamuel <lenilvarghese@gmail.com>
2021-11-17 20:07:46 -0700

    added constants for wasd keys and special keys for canned messages

--- constants.js
+++ constants.js
@@ -1,4 +1,23 @@
 const IP = "localhost";
 const PORT = 50541;
 
-module.exports = { IP, PORT };
+const MOVE_UP = "w";
+const MOVE_LEFT = "a";
+const MOVE_DOWN = "s";
+const MOVE_RIGHT = "d";
+const KEYS = ["m", "y"];
+const MESSAGES = {
+  m: "Say: Mama Mia",
+  y: "Say: Yahoo!",
+};
+
+module.exports = {
+  IP,
+  PORT,
+  MOVE_UP,
+  MOVE_LEFT,
+  MOVE_DOWN,
+  MOVE_RIGHT,
+  MESSAGES,
+  KEYS,
+};
