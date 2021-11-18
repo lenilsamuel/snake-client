314e4752382dddcfbf53df42e148e883a11cb84d
lenilsamuel <lenilvarghese@gmail.com>
2021-11-17 20:08:20 -0700

    moved wasd keys and canned messages to constants.js

--- input.js
+++ input.js
@@ -1,34 +1,37 @@
 let connection;
+const {MOVE_UP, MOVE_LEFT, MOVE_DOWN, MOVE_RIGHT, MESSAGES, KEYS} = require('./constants');
 const setupInput = function (conn) {
   connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  const handleUserInput = function (key) {
-    if (key === "\u0003") {
-      process.exit();
-    }
-    if (key === "w") {
-      connection.write("Move: up");
-    }
-    if (key === "a") {
-      connection.write("Move: left");
-    }
-    if (key === "s") {
-      connection.write("Move: down");
-    }
-    if (key === "d") {
-      connection.write("Move: right");
-    }
-    if (key === "m") {
-      connection.write("Say: Mama Mia!");
-    }
-  };
-
   stdin.on("data", handleUserInput);
-
+  
   return stdin;
-};
+}
 
+const handleUserInput = function (key) {
+  if (key === "\u0003") {
+    process.exit();
+  }
+  if (key === MOVE_UP) {
+    connection.write("Move: up");
+  }
+  if (key === MOVE_LEFT) {
+    connection.write("Move: left");
+  }
+  if (key === MOVE_DOWN) {
+    connection.write("Move: down");
+  }
+  if (key === MOVE_RIGHT) {
+    connection.write("Move: right");
+  }
+  if (KEYS.includes(key)) {
+    connection.write(MESSAGES[key]);
+  }
+  // if (key === 'm') {
+  //   connection.write(MESSAGES[key]);
+  // }
+}
 module.exports = { setupInput };
